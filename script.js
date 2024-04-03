0d3e1e995b77cb0302f4c216bdefe7239565c62c
mdsaifurrahman00 <saifurrahmansaif954@gmail.com>
2024-04-04 00:19:15 +0600

    sticky navbar

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 function teamAlert() {
        alert("This page is upto date")
 }
-
+// custom cursor
 new kursor({
        type: 1,
        color: '#B71347',
        removeDefaultCursor: true,
 });
+// sticky navbar 
+window.onscroll = function() {stickey_navbar()}
+let navbar = document.getElementById("navbar");
+
+function stickey_navbar() {
+       if (window.scrollY > 0) {
+              navbar.classList.add("sticky")
+       }else{
+            navbar.classList.remove("sticky")  
+       }
+}
